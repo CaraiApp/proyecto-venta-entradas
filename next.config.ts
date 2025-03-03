0490620d1f02c77c27391d2c2666079fa732da0f
caraiapp <info@caraimelilla.com>
2025-03-03 19:07:43 +0100

    Descripción de los cambios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Cualquier configuración específica
 };
 
 export default nextConfig;
